c6ca629c227ad0fa7375fd877b1b1a0cf350a544
vitaliiminich <132211797+vitaliiminich@users.noreply.github.com>
2023-06-15 13:26:45 +0300

    Changes path of images fpr sorts page store

--- peach/settings.py
+++ peach/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e^!(!9$$lul+a8#&h)0$=x5&xy%axnvpejvimztadc)r&g4q@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vitalzcu.beget.tech', 'www.vitalzcu.beget.tech', '127.0.0.1']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
